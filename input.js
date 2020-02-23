46e94cfa6c662b032013f50ba0acfa570db5a5ea
alisha2 <alisha@volumetree.com>
2020-02-23 14:48:04 +0530

    initial commit

---
+++ input.js
@@ -0,0 +1,34 @@
+let input = [
+    {
+    "item" : "Apple",
+    "id": 1,
+    "quantity": "6"
+    },
+    {
+    "item": "Orange" ,
+    "id":2,
+    "quantity":"2"
+    },
+    {
+    "item": "Potato",
+    "quantity":"14",
+    "id":3
+    },
+    {
+    "item": "Tomato",
+    "quantity": "3",
+    "id":4
+    },
+    {
+    "item": "Cow Milk",
+    "quantity":"8" ,
+    "id":5
+    },
+    {
+    "item": "Gouda",
+    "quantity": "2",
+    "id":8
+    }
+]
+
+module.exports = input ;
---
+++ manipulate.js
@@ -0,0 +1,11 @@
+// Take input 
+
+let data = require('./data');
+let input = require('./input');
+
+
+let input_length = input.length;
+
+for(var i=0 ; i < input_length; i++){
+
+}
